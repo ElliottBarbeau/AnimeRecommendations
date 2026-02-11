6cde1eff07529f5df24977904fb06516420641e9
ElliottBarbeau <elliottbarbeau@gmail.com>
2026-02-11 14:39:11 -0500

    project skeleton and starting db schemas

---
+++ app/db/enums.py
@@ -0,0 +1,17 @@
+import enum
+
+class Provider(enum.StrEnum):
+    MAL = "mal"
+    ANILIST = "anilist"
+
+class Status(enum.StrEnum):
+    COMPLETED = "completed"
+    AIRING = "airing"
+    ANNOUNCED = "announced"
+
+class AnimeType(enum.StrEnum):
+    OVA = "ova"
+    TV = "tv"
+    ONA = "ona"
+    MOVIE = "movie"
+    SPECIAL = "special"
